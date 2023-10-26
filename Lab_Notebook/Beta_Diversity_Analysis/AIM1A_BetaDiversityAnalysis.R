5e798f9e6b691c6da752f8c060aa3fd9bb9259d0
mairi-macaulay <mairimacaulay@gmail.com>
2023-10-25 20:13:26 -0700

    Changes to Beta Diversity Aim 1A file

--- Lab_Notebook/Beta_Diversity_Analysis/AIM1A_BetaDiversityAnalysis.R
+++ Lab_Notebook/Beta_Diversity_Analysis/AIM1A_BetaDiversityAnalysis.R
@@ -6,39 +6,40 @@ library(ape)
 library(tidyverse)
 library(picante) 
 
-dorms_metadata <- read_delim(file = "dorms_metadata_updated.txt", delim = "\t")
+##Load files##
+dorms_metadata <- read_delim(file = "Lab_Notebook/metadata/dorms_metadata_updated.txt", delim = "\t")
+load(file="Lab_Notebook/Phyloseq/dorms_rare_sheetwashfreq.RData")
 
-##Filter out "NA" values##
-dorms_rare <- subset_samples(dorms_rare, sheetwashfreq_binned != "NA")
+##Removing rows with "na" for sheetwashing frequency##
+dorms_metadata_no_na <- dorms_metadata[complete.cases(dorms_metadata$sheetwashfreq_binned), ]
 
 ##Unweighted Unifrac##
 unifrac_dm <- distance(dorms_rare, method="unifrac")
 
 pcoa_unifrac <- ordinate(dorms_rare, method="PCoA", distance=unifrac_dm)
 
-plot_ordination(dorms_rare, pcoa_unifrac, color = "sheetwashfreq_binned")
-
 gg_pcoa_unifrac <- plot_ordination(dorms_rare, pcoa_unifrac, color = "sheetwashfreq_binned") +
   labs(col="Sheet Wash Frequency")
 gg_pcoa_unifrac
 
+#Statistical Analysis#
 unifrac_dm <- UniFrac(dorms_rare, weighted=FALSE)
-adonis2(unifrac_dm ~ sheet.wash.frequency, data=)
+adonis2(unifrac_dm ~ sheetwashfreq_binned, data=dorms_metadata_no_na)
 
 
 ##Weighted Unifrac##
-wunifrac_dm <- distance(dorms_rare, method="wunifrac")
+wu_dm <- distance(dorms_rare, method="wunifrac")
 
-pcoa_wunifrac <- ordinate(dorms_rare, method="PCoA", distance=wunifrac_dm)
-
-plot_ordination(dorms_rare, pcoa_wunifrac, color = "host_subject_id", shape="sheetwashfreq_binned")
+pcoa_wunifrac <- ordinate(dorms_rare, method="PCoA", distance=wu_dm)
 
 gg_pcoa_wunifrac <- plot_ordination(dorms_rare, pcoa_wunifrac, color = "sheetwashfreq_binned") +
   labs(col="Sheet Wash Frequency")
 gg_pcoa_wunifrac
 
+#Statistical Analysis#
 wunifrac_dm <- UniFrac(dorms_rare, weighted=TRUE)
-adonis2(wunifrac_dm ~ sheet.wash.frequency, data=)
+adonis2(wunifrac_dm ~ sheetwashfreq_binned, data=dorms_metadata_no_na)
+
 
 
 
