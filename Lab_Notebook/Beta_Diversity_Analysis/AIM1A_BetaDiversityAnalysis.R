2b85730ee1e5fd97faf1deb20396aed63ceca441
cwjlee <56702020+cwjlee@users.noreply.github.com>
2023-10-26 23:25:34 -0700

    Chris's solution to beta div stats

--- Lab_Notebook/Beta_Diversity_Analysis/AIM1A_BetaDiversityAnalysis.R
+++ Lab_Notebook/Beta_Diversity_Analysis/AIM1A_BetaDiversityAnalysis.R
@@ -4,14 +4,20 @@
 library(phyloseq)
 library(ape)
 library(tidyverse)
-library(picante) 
+library(vegan)
+
 
 ##Load files##
 dorms_metadata <- read_delim(file = "Lab_Notebook/metadata/dorms_metadata_updated.txt", delim = "\t")
 load(file="Lab_Notebook/Phyloseq/dorms_rare_sheetwashfreq.RData")
 
 ##Removing rows with "na" for sheetwashing frequency##
-dorms_metadata_no_na <- dorms_metadata[complete.cases(dorms_metadata$sheetwashfreq_binned), ]
+dorms_metadata_no_na <- sample_data(dorms_rare)
+
+#Generating diversity metrics
+alphadiv <- estimate_richness(dorms_rare)
+samp_dat_wdiv <- data.frame(dorms_metadata_no_na, alphadiv)
+
 
 ##Unweighted Unifrac##
 unifrac_dm <- distance(dorms_rare, method="unifrac")
@@ -22,9 +28,11 @@ gg_pcoa_unifrac <- plot_ordination(dorms_rare, pcoa_unifrac, color = "sheetwashf
   labs(col="Sheet Wash Frequency")
 gg_pcoa_unifrac
 
+
 #Statistical Analysis#
+set.seed(1) #setting a seed like this ensures the stats test will be repeatable
 unifrac_dm <- UniFrac(dorms_rare, weighted=FALSE)
-adonis2(unifrac_dm ~ sheetwashfreq_binned, data=dorms_metadata_no_na)
+adonis2(unifrac_dm ~ sheetwashfreq_binned, data=samp_dat_wdiv)
 
 
 ##Weighted Unifrac##
@@ -38,7 +46,7 @@ gg_pcoa_wunifrac
 
 #Statistical Analysis#
 wunifrac_dm <- UniFrac(dorms_rare, weighted=TRUE)
-adonis2(wunifrac_dm ~ sheetwashfreq_binned, data=dorms_metadata_no_na)
+adonis2(wunifrac_dm ~ sheetwashfreq_binned, data=samp_dat_wdiv)
 
 
 
