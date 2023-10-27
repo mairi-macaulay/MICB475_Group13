85a29241effb4af62abb501579bf4c942ca91f36
mairi-macaulay <mairimacaulay@gmail.com>
2023-10-27 12:26:54 -0700

    finished plots for beta diversity 1b and added a 1b folder

--- Lab_Notebook/Beta_Diversity_Analysis/AIM_1B/AIM1B_BetaDiversityAnalysis.R
+++ Lab_Notebook/Beta_Diversity_Analysis/AIM_1B/AIM1B_BetaDiversityAnalysis.R
@@ -27,10 +27,15 @@ gg_pcoa_unifrac <- plot_ordination(dorms_rare, pcoa_unifrac, color = "last_showe
   labs(col="Shower Recency") + stat_ellipse()
 gg_pcoa_unifrac
 
-
 #Unweighted Unifrac PERMANOVA Test#
 unifrac_dm <- UniFrac(dorms_rare, weighted=FALSE)
 adonis2(unifrac_dm ~ last_shower_binned, data=samp_dat_wdiv)
+
+#Save plots#
+ggsave(filename = "Lab_Notebook/Beta_Diversity_Analysis/AIM_1B/1B_unweighted_pcoa.png"
+       , gg_pcoa_unifrac
+       , height=4, width=5)
+
 
 
 ##Weighted Unifrac##
@@ -41,28 +46,45 @@ gg_pcoa_wunifrac <- plot_ordination(dorms_rare, pcoa_wunifrac, color = "last_sho
 gg_pcoa_wunifrac
 
 #Weighted Unifrac PERMANOVA Test#
-unifrac_dm <- UniFrac(dorms_rare, weighted=TRUE)
-adonis2(unifrac_dm ~ last_shower_binned, data=samp_dat_wdiv)
+wunifrac_dm <- UniFrac(dorms_rare, weighted=TRUE)
+adonis2(wunifrac_dm ~ last_shower_binned, data=samp_dat_wdiv)
+
+#Save plots#
+ggsave(filename = "Lab_Notebook/Beta_Diversity_Analysis/AIM_1B/1B_weighted_pcoa.png"
+       , gg_pcoa_wunifrac
+       , height=4, width=5)
+
 
 
 ##Jaccard##
 j_dm <- distance(dorms_rare, method = "jaccard", binary = TRUE)
 pcoa_jaccard <- ordinate(dorms_rare, method="PCoA", distance=j_dm)
-plot_ordination(dorms_rare, pcoa_jaccard, color = "last_shower_binned") + labs(col = "Shower Recency") + stat_ellipse()
+gg_pcoa_jaccard <- plot_ordination(dorms_rare, pcoa_jaccard, color = "last_shower_binned") + labs(col = "Shower Recency") + stat_ellipse()
 pcoa_jaccard
 
 #Jaccard diversity PERMANOVA Test#
 dm_jaccard <- vegdist(t(otu_table(dorms_rare)), method="jaccard")
 adonis2(dm_jaccard ~ last_shower_binned, data=samp_dat_wdiv)
 
+#Save plots#
+ggsave(filename = "Lab_Notebook/Beta_Diversity_Analysis/AIM_1B/1B_jaccard_pcoa.png"
+       , gg_pcoa_jaccard
+       , height=4, width=5)
+
+
+
 ##Bray Curtis##
 bc_dm <- distance(dorms_rare, method="bray")
 pcoa_bc <- ordinate(dorms_rare, method="PCoA", distance=bc_dm)
-plot_ordination(dorms_rare, pcoa_bc, color = "last_shower_binned") + labs(col = "Shower Recency") + stat_ellipse()
+gg_pcoa_bc <- plot_ordination(dorms_rare, pcoa_bc, color = "last_shower_binned") + labs(col = "Shower Recency") + stat_ellipse()
 
 #Bray Curtic diversity PERMANOVA Test#
 dm_bray <- vegdist(t(otu_table(dorms_rare)), method="bray")
 adonis2(dm_bray ~ last_shower_binned, data=samp_dat_wdiv)
 
+#Save plots#
+ggsave(filename = "Lab_Notebook/Beta_Diversity_Analysis/AIM_1B/1B_bc_pcoa.png"
+       , gg_pcoa_bc
+       , height=4, width=5)
 
 
