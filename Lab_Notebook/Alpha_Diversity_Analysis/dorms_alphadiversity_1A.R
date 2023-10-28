7238f52553649c9a1f422938ce7c6e0be79a3f0d
abigailcho <abigailcho123@gmail.com>
2023-10-28 14:13:17 -0700

    update 1A and 1B alpha scripts

--- Lab_Notebook/Alpha_Diversity_Analysis/dorms_alphadiversity_1A.R
+++ Lab_Notebook/Alpha_Diversity_Analysis/dorms_alphadiversity_1A.R
@@ -6,44 +6,135 @@ library(tidyverse)
 library(picante)
 
 ##Load in RData
-load("dorms_rare.RData")
-load("dorms_final.RData")
-
-#remove NAs
-dorms_rare <- subset_samples(dorms_rare, !is.na(sheetwashfreq_binned))
+load("../Phyloseq/dorms_rare_sheetwashfreq.RData")
+load("../Phyloseq/dorms_final_sheetwashfreq.RData")
 
 #### Alpha diversity ######
 #view all metrics
 plot_richness(dorms_rare)
 
-# define which of the measures you want to show
-plot_richness(dorms_rare, measures = c("Shannon","Chao1"))
+#extract information
+alphadiv <- estimate_richness(dorms_rare)
+samp_dat <- sample_data(dorms_rare) #extract out metadata
+samp_dat_wdiv <- data.frame(samp_dat, alphadiv)
 
+#####Shannon#####
 # save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
-gg_richness <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("Shannon","Chao1")) +
+gg_shannon_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("Shannon")) +
   xlab("Sheet Wash Frequency") +
   geom_boxplot()
-gg_richness
+gg_shannon_sheetwashfreq
+
+#ANOVA
 
 # save plot file 
-ggsave(filename = "sheetwashfreq_plot_richness.png"
-       , gg_richness
+ggsave(filename = "sheetwashfreq_plot_shannon.png"
+       , gg_shannon_sheetwashfreq
        , height=4, width=6)
 
-#tells you all the measures 
-estimate_richness(dorms_rare)
+#####Observed##### 
+# save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
+gg_observed_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("Observed")) +
+  xlab("Sheet Wash Frequency") +
+  geom_boxplot()
+gg_observed_sheetwashfreq
 
-## Phylogenetic diversity
+#ANOVA
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_observed.png"
+       , gg_observed_sheetwashfreq
+       , height=4, width=6)
+
+#####Chao1##### 
+# save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
+gg_chao1_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("Chao1")) +
+  xlab("Sheet Wash Frequency") +
+  geom_boxplot()
+gg_chao1_sheetwashfreq
+
+#ANOVA
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_chao1.png"
+       , gg_chao1_sheetwashfreq
+       , height=4, width=6)
+
+#####ACE##### 
+# save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
+gg_ace_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("ACE")) +
+  xlab("Sheet Wash Frequency") +
+  geom_boxplot()
+gg_ace_sheetwashfreq
+
+#ANOVA
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_ace.png"
+       , gg_ace_sheetwashfreq
+       , height=4, width=6)
+
+#####Simpson##### 
+# save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
+gg_simpson_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("Simpson")) +
+  xlab("Sheet Wash Frequency") +
+  geom_boxplot()
+gg_simpson_sheetwashfreq
+
+#ANOVA
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_simpson.png"
+       , gg_simpson_sheetwashfreq
+       , height=4, width=6)
+
+#####InvSimpson##### 
+# save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
+gg_invsimpson_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("InvSimpson")) +
+  xlab("Sheet Wash Frequency") +
+  geom_boxplot()
+gg_invsimpson_sheetwashfreq
+
+#ANOVA
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_invsimpson.png"
+       , gg_invsimpson_sheetwashfreq
+       , height=4, width=6)
+
+#####Fisher##### 
+# save into object, define x-axis as sheetwashfreq_binned and relabel, add a boxplot
+gg_fisher_sheetwashfreq <- plot_richness(dorms_rare, x = "sheetwashfreq_binned", measures = c("Fisher")) +
+  xlab("Sheet Wash Frequency") +
+  geom_boxplot()
+gg_fisher_sheetwashfreq
+
+#ANOVA
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_fisher.png"
+       , gg_fisher_sheetwashfreq
+       , height=4, width=6)
+
+#####Phylogenetic diversity#####
 # calculate Faith's phylogenetic diversity as PD
-phylo_dist <- pd(t(otu_table(dorms_rare)), phy_tree(dorms_rare),
+phylo_dist_sheetwashfreq <- pd(t(otu_table(dorms_rare)), phy_tree(dorms_rare),
                  include.root=F)
 # add PD to metadata table
-sample_data(dorms_rare)$PD <- phylo_dist$PD
+sample_data(dorms_rare)$PD <- phylo_dist_sheetwashfreq$PD
 # plot any metadata category against the PD
 plot.pd <- ggplot(sample_data(dorms_rare), aes(sheetwashfreq_binned, PD)) +
   geom_boxplot() +
   xlab("Sheet Washing Frequency") +
   ylab("Phylogenetic Diversity")
-
 # view plot
-plot.pd
+plot.pd_sheetwashfreq
+
+#ANOVA
+
+
+# save plot file 
+ggsave(filename = "sheetwashfreq_plot_pd.png"
+       , plot.pd_sheetwashfreq
+       , height=4, width=6)
+
