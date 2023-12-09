d9c08a160f069cf3a29bb6f9e0b8dc6f412f9c1a
LinaAnwari <106774733+LinaAnwari@users.noreply.github.com>
2023-12-08 23:41:25 -0800

    Update sheetwashfreq_genus.R

--- Lab_Notebook/Taxa_Barplots/AIM 1A/sheetwashfreq_genus.R
+++ Lab_Notebook/Taxa_Barplots/AIM 1A/sheetwashfreq_genus.R
@@ -1,3 +1,4 @@
+# Load libraries
 library(phyloseq)
 library(tidyverse)
 library(ggplot2)
@@ -5,23 +6,23 @@ library(ape)
 library(vegan)
 
 
-# Load Rdata
+# Load Rdata (phyloseq object)
 load("../../Phyloseq/dorms_final_sheetwashfreq.RData")
 
 
 
-# Extracting OTU data
+# Extract OTU data from the phyloseq object
 otu_table <- data.frame(t(otu_table(dorms_final)))
 otu_table$ID <- rownames(otu_table)
 
-# Extracting metadata
+# Extract metadata from the phyloseq object
 metadata <- data.frame(sample_data(dorms_final))
 metadata$ID <- rownames(metadata)
 
 # Load the raw taxonomy file
 tax <- data.frame(tax_table(dorms_final))
 
-# Formatting the taxa dataframe and cleaning names
+# Format the taxa dataframe and clean names
 tax_mat <- tax[,-1]
 tax_mat <- data.frame(tax_mat)
 tax_mat$Phylum <- gsub("^...","",tax_mat$Phylum)
@@ -32,25 +33,27 @@ tax_mat$Genus <- gsub("^...","",tax_mat$Genus)
 tax_mat$Species <- gsub("^...","",tax_mat$Species)
 tax_mat$ASV <- rownames(tax_mat)
 
-# Joining OTU and metadata and taxanomic information
+# Join OTU and metadata and taxanomic information
 otu_meta <- inner_join(metadata, otu_table, by = "ID")
 
-#transforming the OTU matrix to a single column called abundance. 27 represents the number of metadata columns we wnat to exlcude
+# Transform the OTU matrix to a single column called abundance. 27 represents the number of metadata columns we wnat to exlcude
 grouped = gather(otu_meta, key = "ASV", value = "abundance", -(1:27))
 
-#Joining the taxa information with otu_meta
+# Joining the taxa information with otu_meta
 grouped_taxa = inner_join(tax_mat, grouped, by = "ASV", multiple = "all")
 
+# Generate a new column containing sheet washing and taxa data
+grouped_taxa$legend = paste(grouped_taxa$sheetwashfreq_binned) 
 
-grouped_taxa$legend = paste(grouped_taxa$sheetwashfreq_binned) #Can add gender for when gender becomes a consideration
+
 #Generating relative abundance at the genus level
 
-#Define the low, medium, and high levels
+# Define the low, medium, and high levels
 levels <- unique(grouped_taxa$sheetwashfreq_binned)
-#Create a new empty dataframe to put firmicute data in.
+# Create a new empty dataframe to put firmicute data in.
 data_rel_genus = data.frame()
 
-#Run a loop to generate the relative abundance of each genus for low, medium, and high groups.
+# Run a loop to generate the relative abundance of each genus for low, medium, and high groups.
 for (i in levels){
   
   df = grouped_taxa %>%
@@ -68,15 +71,16 @@ for (i in levels){
   
 }
 
-#Filter for only Proteobacteria - repeat this same step for the other phylum but replace "Proteobacteria" with Firmicutes, Actinobacteriota, Fusobacteriota, and Bacteroidota
+# Filter for only Proteobacteria - repeat this same step for the other phylum but replace "Proteobacteria" with Firmicutes, Actinobacteriota, Fusobacteriota, and Bacteroidota
 data_rel_proteobacteria = data_rel_genus %>%
-  filter(Phylum == "Proteobacteria") %>% #Keep only bacteria that are in the Proteobacteria phylum. Change this line for different phyla.
+  filter(Phylum == "Proteobacteria") %>% # Keep only bacteria that are in the Proteobacteria phylum. Change this line for different phyla.
   group_by(legend,Phylum, Order, Class, Family,Genus) %>%
-  summarise(mean_rel_abs = sum(rel_abs))%>% #Add relative abundances together for multiple species that have the same genus
-  filter(mean_rel_abs>= 1) #Remove Genus that have a relative abundance less than 1%
+  summarise(mean_rel_abs = sum(rel_abs))%>% # Add relative abundances together for multiple species that have the same genus
+  filter(mean_rel_abs>= 1) # Remove genus that have a relative abundance less than 1%
 
-data_rel_proteobacteria$legend = factor(data_rel_proteobacteria$legend, levels = c("low","high")) #create the order for low, medium, high in the plot
-ggplot(data =data_rel_proteobacteria, aes(legend,mean_rel_abs, fill = Family))+#Generating the plot with X axis equal to sheetwash_freq_binned
+# Use ggplot2 to generate taxa bar plots at the genus level for the phyla of interest (Proteobacteria, Firmicutes, Actinobacteriota, Fusobacteriota, Bacteroidota)
+data_rel_proteobacteria$legend = factor(data_rel_proteobacteria$legend, levels = c("low","high")) # Create the order for low, medium, high in the plot
+ggplot(data =data_rel_proteobacteria, aes(legend,mean_rel_abs, fill = Family))+ # Generate the plot with X axis equal to sheetwash_freq_binned
   labs(x = "Sheet Washing Frequency", y = "Relative Abundance")+
   geom_col()+
   theme_bw()+
